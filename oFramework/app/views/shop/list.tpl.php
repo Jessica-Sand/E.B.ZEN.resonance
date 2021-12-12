08f2d73c05f1c88cb4ac1aed6e1bdf81aa160c20
Sand Jessica <jessica.sand.mjm@gmail.com>
2021-12-12 19:13:49 +0100

    Adding paddings and margins for product list

--- oFramework/app/views/shop/list.tpl.php
+++ oFramework/app/views/shop/list.tpl.php
@@ -17,22 +17,22 @@
 
     <section class="products-grid bg-white">
         <div class="container-fluid">
-            <div class="row text-center">
+            <div class="row text-center mb-4">
                 <p>Les voici enfin… côté boutique EB.ZEN les coffrets et idées cadeaux, disponible tout au long de l'année; <br/>lors d'un anniversaire , à offrir ou se faire plaisir!</p>
                 <p>Produits Avril bio, des huiles essentielles en provenance de Corse , et autres coups de cœurs...</p>
                 <p>possibilité de récupérer en DRIVE sur RDV ou envoi par la poste</p>
-                <p>Appelez au <strong>06 70 48 52 95</strong>.</p>
+                <p>Appelez au <strong class="fs-2"><a class="text-body" href="tel:06.70.48.52.95" id="tel-number">06 70 48 52 95</a></strong>.</p>
             </div>
             <div class="row">
             <?php foreach($products as $product): ?>
                 <!-- product-->
-                <div class="product col-xl-3 col-lg-4 col-sm-6 pb-5">
+                <div class="product col-xl-3 col-lg-4 col-sm-6 mb-5 pt-2">
                     <div class="product-image">
                         <a href="<?= $router->generate('shop-details', ['id'=> $product->getId()]) ?>" class="product-hover-overlay-link">
                             <img src="<?=$assetsBaseUri ?>images/<?= $product->getPicture() ?>" alt="product" class="img-fluid">
                         </a>
                     </div>
-                    <div class="product-action-buttons">
+                    <div class="product-action-buttons mt-2">
                         <!-- <a href="#" class="btn btn-outline-dark btn-product-left">
                             <i class="fa fa-shopping-cart"></i>
                         </a> -->
